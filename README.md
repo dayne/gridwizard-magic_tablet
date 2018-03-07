44ff8ced7719da81b3bb40e8bfd494568ea60ea2
Dayne Broderson <broderson@gmail.com>
2018-03-07 10:02:41 -0900

    basic frontend prototype skeleton done

---
+++ README.md
@@ -0,0 +1,14 @@
+## 
+
+Grid Wizard web helper
+
+### basic setup
+
+```
+apt install mosquitto mosquitto-clients
+service enable mosquitto
+bundle
+rackup
+```
+
+http://localhost:9292
---
+++ public/js/gridTable.js
@@ -0,0 +1,19 @@
+/* onmouseover */
+function gridHover(x,y) {
+  /* alert('hover on ' + x + ',' + y); /* verfied works */ 
+}
+
+/* oncontextmenu - right click */
+function gridAlt(x,y) {
+  alert('right client on ' + x + ',' + y); /* verified works */
+}
+
+/* onclick - left click */
+function gridSelect(x,y) {
+  /* alert('left client on ' + x + ',' + y); /* verified works */
+}
+
+/* double click */
+function gridRun(x,y) {
+  alert('double click - run ' + x + ',' + y);
+}
